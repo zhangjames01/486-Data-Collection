3495c5ea399db7264937d5f67fef09bb76197d91
henrlin24 <henrlin@umich.edu>
2023-03-25 14:44:51 -0400

    Update to basic json extraction

--- data_analysis.py
+++ data_analysis.py
@@ -6,9 +6,7 @@ import math
 
 # TODO - dict of users, in the form of { "user_id" : [{ attribute: value }] }
 # probably best to store posts within the dict of users, like a dict of dict
-
-# Stopgap - list of posts dicts
-posts = []
+users = {}
 
 # TODO - break up data analysis into helper functions
 
@@ -16,6 +14,9 @@ posts = []
 def main():
     # get data directory filepath
     datafolder_Path = os.path.join(os.getcwd(), "Gamer_Data/")
+
+    # global declarations
+    global users
 
     # iterate through user data files
     for user_filename in os.listdir(datafolder_Path):
@@ -31,12 +32,18 @@ def main():
             # parses to internal posts dict (based on debug state)
             # continues to next user file if entry does not exists (im looking at you, user_id 102)
             posts_dict_parent = userdata_json.get("data", None)
-            if (posts_dict == None): 
+            if (posts_dict_parent == None or posts_dict_parent == {}): 
                 continue
             posts_dict = posts_dict_parent.get("children", None)
-            if (posts_dict == None): 
+            if (posts_dict == None or posts_dict == []): 
                 continue
 
+            # create entry in users dict
+            # TODO - if desired, can use username as key instead
+            users[user_id] = users.get(user_id, {})
+            user_dict = users[user_id]
+
+            post_index = 0
             # extract relevant post information
             for post_dict in posts_dict:
                 post_data = post_dict.get("data", None)
@@ -44,9 +51,18 @@ def main():
                     continue
                 
                 # extracted info for each post
-                subreddit_name = post_data.get("subreddit", None)
+                subreddit = post_data.get("subreddit", None)
                 post_title = post_data.get("link_title", None)
                 post_content = post_data.get("body", None)
+
+                # save into users dict
+                user_dict[post_index] = user_dict.get(post_index, {})
+                user_dict[post_index]["subreddit"] = user_dict[post_index].get("subreddit", subreddit)
+                user_dict[post_index]["post_title"] = user_dict[post_index].get("post_title", post_title)
+                user_dict[post_index]["post_content"] = user_dict[post_index].get("post_content", post_content)
+
+                post_index += 1
+
 
 if __name__ == '__main__':
     main()
