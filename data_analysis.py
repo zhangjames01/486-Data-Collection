7b2fdd92eaa5a48db1c7e4813e4646f91d4fa82f
QuixoticJk <jlkrolik@umich.edu>
2023-04-11 17:32:41 -0400

    Updated data_analysis with better tokenizing and lemmatizing

--- data_analysis.py
+++ data_analysis.py
@@ -3,13 +3,23 @@ import os
 import json
 import re
 import math
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+from nltk.corpus import stopwords
+
+
+stops = set(stopwords.words('english'))
+# print(stops)
+lemmatizer = WordNetLemmatizer()
 
 def main():
-    bad_words = []
+    bad_words = set()
     with open("list-of-bad-words.txt", 'r', encoding='iso-8859-1') as f:
         data = json.load(f)
     for word in data["words"]:
-        bad_words.append(str(word))
+        bad_words.add(str(word))
+        bad_words.add(lemmatizer.lemmatize(str(word)))
+    
     print(bad_words)
 
     GamerResultFile = open("gamerSlurResults.txt", "w")
@@ -17,25 +27,53 @@ def main():
     slurs = 0
     words = 0
     with open('all_gamer_words.txt','r') as file:
-        for line in file:
-            for word in line.split(" "):
-                words += 1
-                if word in bad_words:
-                    print(word)
-                    slurs += 1
+        text = file.read().lower()
+        text = removeSGML(text)
+        text = tokenizeText(text)
+        for word in text:
+            if(word in stops):
+                continue
+            word = lemmatizer.lemmatize(word)
+            words += 1
+            if word in bad_words:
+                print(word)
+                slurs += 1
     GamerResultFile.write("number of words: " + str(words) +"\nnumber of Slurs: " +str(slurs) +"\npercentage: "+str(slurs/words * 100)+"%")
     
     slurs = 0
     words = 0
     with open('all_words.txt','r') as file:
-        for line in file:
-            for word in line.split(" "):
-                words += 1
-                if word in bad_words:
-                    print(word)
-                    slurs += 1
+        text = file.read().lower()
+        text = removeSGML(text)
+        text = tokenizeText(text)
+        for word in text:
+            if(word in stops):
+                continue
+            word = lemmatizer.lemmatize(word)
+            words += 1
+            if word in bad_words:
+                print(word)
+                slurs += 1
     allResultFile.write("number of words: " + str(words) +"\nnumber of Slurs: " +str(slurs) +"\npercentage: "+str(slurs/words * 100)+ "%")
     
+def removeSGML(text):
+    return re.sub(r'<.*?>', '', text)
+
+def tokenizeText(text):
+    # Account for acronyms and abbreviations
+    text = re.sub(r"([A-Za-z]\.)+", r"\1", text)
+    # Account for numbers
+    text = re.sub(r"(\d+)([,/.])(\d+)", r"\1\2\3", text)
+    # Account for dates
+    text = re.sub(r"(\d{1,2}[/-]){2}\d{4}", r"\1", text)
+    # Account for possessive
+    text = re.sub(r"(\w+)(')(s)", r"\1 \2\3", text)
+    # Account for hyphenated words
+    text = re.sub(r"(\w+)(-)(\w+)", r"\1 \2 \3", text)
+    # Tokenize text
+    tokens = re.findall(r"[\w']+(?:[-.']\w+)*|[.,!?;]", text)
+    tokens = [token for token in tokens if token not in ",.!?;"]
+    return tokens
     
 
 
