bae6642eb4ce0827813631fe83ff2aab86757a88
henrlin24 <henrlin@umich.edu>
2023-04-12 15:43:57 -0400

    Categorized data analysis finished
    Outputs included as well, overwrites old result files

--- data_analysis_categorized.py
+++ data_analysis_categorized.py
@@ -14,6 +14,15 @@ stops = set(stopwords.words('english'))
 lemmatizer = WordNetLemmatizer()
 
 def main():
+    # GENERAL - reading in list of words
+    bad_words = set()
+    with open("list-of-bad-words.txt", 'r', encoding='iso-8859-1') as f:
+        data = json.load(f)
+    for word in data["words"]:
+        bad_words.add(str(word))
+        bad_words.add(lemmatizer.lemmatize(str(word)))
+
+    # CATEGORIZED - reading in list of words by category
     # get data directory filepath
     datafolder_Path = os.path.join(os.getcwd(), "bad_words_by_category/")
 
@@ -30,10 +39,15 @@ def main():
             badwords_dict[classType].add(str(word))
             badwords_dict[classType].add(lemmatizer.lemmatize(str(word)))
 
+
+    # DATA ANALYSIS - Getting counts and return the data analyst statistics ----------------------------------------------------
     GamerResultFile = open("gamerSlurResults.txt", "w")
-    allResultFile = open("allSlurResults.txt", "w")
-    slurs = 0
-    words = 0
+    GeneralResultFile = open("allSlurResults.txt", "w")
+
+    # GAMER ---------------------------------------------------------------------
+    words_gamer = 0
+    slurs_gamer = 0
+    slurs_count_by_category_gamer = {}
     with open('all_gamer_words.txt','r') as file:
         text = file.read().lower()
         text = removeSGML(text)
@@ -42,14 +56,26 @@ def main():
             if(word in stops):
                 continue
             word = lemmatizer.lemmatize(word)
-            words += 1
+            words_gamer += 1
+            # update general number of slurs
             if word in bad_words:
-                print(word)
-                slurs += 1
-    GamerResultFile.write("number of words: " + str(words) +"\nnumber of Slurs: " +str(slurs) +"\npercentage: "+str(slurs/words * 100)+"%")
+                slurs_gamer += 1
+            # update count of slurs by category
+            for category in badwords_dict:
+                if word in badwords_dict[category]:
+                    # print(str(word) + " in " + str(category))
+                    slurs_count_by_category_gamer[category] = slurs_count_by_category_gamer.get(category, 0) + 1
+                    break
+    # OUTPUT 
+    GamerResultFile.write("number of words: " + str(words_gamer) + "\n")
+    GamerResultFile.write("number of slurs: " + str(slurs_gamer) +"\npercentage: "+ str(slurs_gamer/words_gamer * 100) + "% \n")
+    for category in badwords_dict:
+        GamerResultFile.write("\nnumber of slurs for category " + str(category) + ": " + str(slurs_count_by_category_gamer[category]) +"\npercentage: "+str(slurs_count_by_category_gamer[category]/words_gamer * 100)+"%")
     
-    slurs = 0
-    words = 0
+    # GENERAL ---------------------------------------------------------------------
+    words_general = 0
+    slurs_general = 0
+    slurs_count_by_category_general = {}
     with open('all_words.txt','r') as file:
         text = file.read().lower()
         text = removeSGML(text)
@@ -58,11 +84,21 @@ def main():
             if(word in stops):
                 continue
             word = lemmatizer.lemmatize(word)
-            words += 1
+            words_general += 1
+            # update general number of slurs
             if word in bad_words:
-                print(word)
-                slurs += 1
-    allResultFile.write("number of words: " + str(words) +"\nnumber of Slurs: " +str(slurs) +"\npercentage: "+str(slurs/words * 100)+ "%")
+                slurs_general += 1
+            # update count of slurs by category
+            for category in badwords_dict:
+                if word in badwords_dict[category]:
+                    # print(str(word) + " in " + str(category))
+                    slurs_count_by_category_general[category] = slurs_count_by_category_general.get(category, 0) + 1
+                    break
+    # OUTPUT 
+    GeneralResultFile.write("number of words: " + str(words_general) + "\n")
+    GeneralResultFile.write("number of slurs: " + str(slurs_general) +"\npercentage: "+ str(slurs_general/words_general * 100) + "% \n")
+    for category in badwords_dict:
+        GeneralResultFile.write("\nnumber of slurs for category " + str(category) + ": " + str(slurs_count_by_category_general[category]) +"\npercentage: "+str(slurs_count_by_category_general[category]/words_general * 100)+"%")
     
 def removeSGML(text):
     return re.sub(r'<.*?>', '', text)
